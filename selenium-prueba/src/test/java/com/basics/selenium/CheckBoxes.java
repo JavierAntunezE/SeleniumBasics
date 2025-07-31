3cd6aec60f16d228aee0f7218809ed67c12c8522
javier-rrkd\student <javier.ant.4@gmail.com>
2025-07-31 15:01:40 -0400

    Revert "Se agrega un print de confirma al cerrar driver"
    This reverts commit 459d194fdab0b3bf4cbab71a18f83d1b7d257b5d.

--- selenium-prueba/src/test/java/com/basics/selenium/CheckBoxes.java
+++ selenium-prueba/src/test/java/com/basics/selenium/CheckBoxes.java
@@ -40,7 +40,6 @@ public class CheckBoxes {
         try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
 
         driver.quit();
-        System.out.println("Cierra el driver");
 	}
 
 }
